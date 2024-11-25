f77e930f1be3f530e8ff770c788caac8655f7a0d
Georgy Kapelyushok <kapgosha@gmail.com>
2024-11-26 00:06:17 +0300

    feat: DeStaking done

--- tests/DeStaking.spec.ts
+++ tests/DeStaking.spec.ts
@@ -1,10 +1,167 @@
 import { Blockchain, printTransactionFees, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { Address, beginCell, toNano } from '@ton/core';
+import { beginCell, toNano } from '@ton/core';
 import { StakingMaster } from '../wrappers/StakingMaster';
 import { StakingChild } from '../build/DeStaking/tact_StakingChild';
 import { Token } from '../wrappers/Token';
 import { TokenWallet } from '../wrappers/TokenWallet';
-import { JettonMinter, jettonContentToCell } from '../wrappers/FuncToken';
-import { JettonWallet } from '../wrappers/FuncJettonWallet';
 import '@ton/test-utils';
-import { compile } from '@ton/blueprint';
+
+
+describe('Staking', () => {
+    let blockchain: Blockchain;
+    let deployer: SandboxContract<TreasuryContract>;
+    let staker: SandboxContract<TreasuryContract>;
+    let stakingMasterContract: SandboxContract<StakingMaster>;
+    let stakingChildContract: SandboxContract<StakingChild>;
+
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+        blockchain.now = 500;
+        deployer = await blockchain.treasury('deployer', {balance: toNano('100')});
+
+    });
+    
+    it("should claim reward my token", async () => {
+        staker = await blockchain.treasury('staker', {balance: toNano('100')});
+
+        let token = blockchain.openContract(await Token.fromInit(deployer.address, null, 10n));
+        const tokenDeployResult = await token.send(
+            deployer.getSender(),
+            {
+                value: toNano('10'),
+            },
+            {
+                $$type: 'Mint',
+                amount: 10000n
+            }
+        );
+
+        await token.send(
+            staker.getSender(),
+            {
+                value: toNano('10'),
+            },
+            {
+                $$type: 'Mint',
+                amount: 10000n
+            }
+        );
+        const stakerWallet = blockchain.openContract(TokenWallet.fromAddress(
+                await token.getGetWalletAddress(staker.address)
+        ));
+        const deployerWallet = blockchain.openContract(TokenWallet.fromAddress(
+            await token.getGetWalletAddress(deployer.address)
+        ));
+
+        console.log("Deploy token and mint to deployer: ");
+        // printTransactionFees(tokenDeployResult.transactions);
+
+        expect(tokenDeployResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: token.address,
+            deploy: true,
+            success: true,
+        });
+        expect(tokenDeployResult.transactions).toHaveTransaction({
+            from: token.address,
+            to: deployerWallet.address,
+            deploy: true,
+            success: true,
+        });
+        expect(tokenDeployResult.transactions).toHaveTransaction({
+            from: deployerWallet.address,
+            to: deployer.address,
+            success: true
+        });
+        expect((await stakerWallet.getGetWalletData()).balance).toEqual(10000n);
+        
+        stakingMasterContract = blockchain.openContract(await StakingMaster.fromInit(deployer.address, 1000n));
+        stakingChildContract = blockchain.openContract(await StakingChild.fromInit(stakingMasterContract.address, staker.address));
+        let stakingWallet = blockchain.openContract(TokenWallet.fromAddress(
+            await token.getGetWalletAddress(stakingMasterContract.address)
+        ));
+        await stakingMasterContract.send(deployer.getSender(), { value: toNano("1") }, null);
+        await deployerWallet.send(
+            deployer.getSender(),
+            { value: toNano('10') },
+            {
+                $$type: "TokenTransfer",
+                query_id: 0n,
+                amount: 500n,
+                destination: stakingMasterContract.address,  
+                response_destination: deployer.address,
+                custom_payload: null,
+                forward_ton_amount: toNano('0.2'),
+                forward_payload: beginCell().endCell().asSlice(),
+            }
+        );
+        const transferResult = await stakerWallet.send(
+            staker.getSender(),
+            { value: toNano('10') },
+            {
+                $$type: "TokenTransfer",
+                query_id: 0n,
+                amount: 100n,
+                destination: stakingMasterContract.address,  
+                response_destination: staker.address,
+                custom_payload: null,
+                forward_ton_amount: toNano('0.2'),
+                forward_payload: beginCell().endCell().asSlice(),
+            }
+        );
+        // printTransactionFees(transferResult.transactions);
+        expect(transferResult.transactions).toHaveTransaction({
+            from: stakerWallet.address,
+            to: stakingWallet.address,
+            success: true,
+        }); 
+
+        // console.log(await stakingChildContract.getAmount());
+        // console.log(await stakingChildContract.getReward());
+        // console.log((await stakingWallet.getGetWalletData()).balance);
+
+        blockchain.now = 100000000;
+
+        // console.log(await stakingChildContract.getAmount());
+        // console.log(await stakingChildContract.getReward());
+        // console.log((await stakingWallet.getGetWalletData()).balance);
+
+        const stakerBalanceBefore = (await stakerWallet.getGetWalletData()).balance
+        const res = await stakingChildContract.send(staker.getSender(), { value: toNano("1")},
+            {
+                $$type: "ClaimReward",
+            }
+        );
+        // console.log(await stakingChildContract.getAmount());
+        // console.log(await stakingChildContract.getReward());
+        // console.log((await stakingWallet.getGetWalletData()).balance);
+
+
+        const apr = (amount: bigint, percent: bigint, durationTime: bigint) => { 
+            return amount * percent * durationTime / (365n * 24n * 3600n * 10000n);
+        };
+        const stakerBalanceAfter = (await stakerWallet.getGetWalletData()).balance;
+        let reward = apr(90n, 1000n, 100000000n - 500n);
+        reward -= reward / 10n;
+        expect(stakerBalanceAfter).toEqual(stakerBalanceBefore + reward);
+
+        blockchain.now = 200000000;
+
+        // console.log(await stakingChildContract.getAmount());
+        // console.log(await stakingChildContract.getReward());
+        // console.log((await stakingWallet.getGetWalletData()).balance);
+
+        const stakerBalanceBefore2 = (await stakerWallet.getGetWalletData()).balance
+        const res2 = await stakingChildContract.send(staker.getSender(), { value: toNano("1")},
+            {
+                $$type: "Unstake",
+            }
+        );
+
+
+        const stakerBalanceAfter2 = (await stakerWallet.getGetWalletData()).balance;
+        let reward2 = apr(90n, 1000n, 100000000n) + 90n;
+        reward2 -= reward2 / 10n;
+        expect(stakerBalanceAfter2).toEqual(stakerBalanceBefore2 + reward2);
+    });
+});
--- tests/Staking.spec.ts
+++ tests/Staking.spec.ts
@@ -11,6 +11,7 @@ import { compile } from '@ton/blueprint';
 describe('Staking', () => {
     let blockchain: Blockchain;
     let deployer: SandboxContract<TreasuryContract>;
+    let staker: SandboxContract<TreasuryContract>;
     let stakingContract: SandboxContract<StakingContract>;
 
     beforeEach(async () => {
@@ -21,6 +22,8 @@ describe('Staking', () => {
     });
     
     it("should claim reward my token", async () => {
+        staker = await blockchain.treasury('staker', {balance: toNano('100')});
+
         let token = blockchain.openContract(await Token.fromInit(deployer.address, null, 10n));
         const tokenDeployResult = await token.send(
             deployer.getSender(),
@@ -32,10 +35,23 @@ describe('Staking', () => {
                 amount: 10000n
             }
         );
-        const deployerWallet = blockchain.openContract(TokenWallet.fromAddress(
-                await token.getGetWalletAddress(deployer.address)
+
+        await token.send(
+            staker.getSender(),
+            {
+                value: toNano('10'),
+            },
+            {
+                $$type: 'Mint',
+                amount: 10000n
+            }
+        );
+        const stakerWallet = blockchain.openContract(TokenWallet.fromAddress(
+                await token.getGetWalletAddress(staker.address)
         ));
-        
+        const deployerWallet = blockchain.openContract(TokenWallet.fromAddress(
+            await token.getGetWalletAddress(deployer.address)
+        ));
 
         console.log("Deploy token and mint to deployer: ");
         printTransactionFees(tokenDeployResult.transactions);
@@ -57,21 +73,20 @@ describe('Staking', () => {
             to: deployer.address,
             success: true
         });
-        expect((await deployerWallet.getGetWalletData()).balance).toEqual(10000n);
+        expect((await stakerWallet.getGetWalletData()).balance).toEqual(10000n);
         
         stakingContract = blockchain.openContract(await StakingContract.fromInit(deployer.address, 1000n));
         let stakingWallet = blockchain.openContract(TokenWallet.fromAddress(
             await token.getGetWalletAddress(stakingContract.address)
         ));
-
         await stakingContract.send(deployer.getSender(), { value: toNano("1") }, null);
-        const transferResult = await deployerWallet.send(
+        await deployerWallet.send(
             deployer.getSender(),
             { value: toNano('10') },
             {
                 $$type: "TokenTransfer",
                 query_id: 0n,
-                amount: 100n,
+                amount: 500n,
                 destination: stakingContract.address,  
                 response_destination: deployer.address,
                 custom_payload: null,
@@ -79,20 +94,33 @@ describe('Staking', () => {
                 forward_payload: beginCell().endCell().asSlice(),
             }
         );
+        const transferResult = await stakerWallet.send(
+            staker.getSender(),
+            { value: toNano('10') },
+            {
+                $$type: "TokenTransfer",
+                query_id: 0n,
+                amount: 100n,
+                destination: stakingContract.address,  
+                response_destination: staker.address,
+                custom_payload: null,
+                forward_ton_amount: toNano('0.2'),
+                forward_payload: beginCell().endCell().asSlice(),
+            }
+        );
         printTransactionFees(transferResult.transactions);
         expect(transferResult.transactions).toHaveTransaction({
-            from: deployerWallet.address,
+            from: stakerWallet.address,
             to: stakingWallet.address,
-            deploy: true,
             success: true,
         }); 
 
-        console.log(await stakingContract.getStakeAmount(0n));
-        console.log((await stakingWallet.getGetWalletData()).balance);
+        // console.log(await stakingContract.getStakeAmount(0n));
+        // console.log((await stakingWallet.getGetWalletData()).balance);
 
         blockchain.now = 100000000;
-        const deployerBalanceBefore = (await deployerWallet.getGetWalletData()).balance
-        const res = await stakingContract.send(deployer.getSender(), { value: toNano("1")},
+        const stakerBalanceBefore = (await stakerWallet.getGetWalletData()).balance
+        const res = await stakingContract.send(staker.getSender(), { value: toNano("1")},
             {
                 $$type: "ClaimReward",
                 indexId: 0n,
@@ -102,8 +130,25 @@ describe('Staking', () => {
         const apr = (amount: bigint, percent: bigint, durationTime: bigint) => { 
             return amount * percent * durationTime / (365n * 24n * 3600n * 10000n);
         };
-        const deployerBalanceAfter = (await deployerWallet.getGetWalletData()).balance;
+        const stakerBalanceAfter = (await stakerWallet.getGetWalletData()).balance;
         const reward = apr(100n, 1000n, 100000000n - 500n);
-        expect(deployerBalanceAfter).toEqual(deployerBalanceBefore + reward);
+        expect(stakerBalanceAfter).toEqual(stakerBalanceBefore + reward);
+
+        blockchain.now = 200000000;
+        const stakerBalanceBefore2 = (await stakerWallet.getGetWalletData()).balance
+        const res2 = await stakingContract.send(staker.getSender(), { value: toNano("1")},
+            {
+                $$type: "Unstake",
+                indexId: 0n,
+            }
+        );
+        const stakerBalanceAfter2 = (await stakerWallet.getGetWalletData()).balance;
+        const reward2 = apr(100n, 1000n, 100000000n);
+        
+        console.log(await stakingContract.getStakeAmount(0n));
+        console.log((await stakingWallet.getGetWalletData()).balance);
+        console.log(stakerBalanceAfter2);
+        console.log(stakerBalanceBefore2 + 90n + reward2);
+        expect(stakerBalanceAfter2).toEqual(stakerBalanceBefore2 + 90n + reward2);
     });
 });
